1218ec9aaf7e7886c4eee5be8966fce362105977
Galymzhan Muratov <galimzhanmuratv@gmail.com>
2022-07-11 19:51:11 +0600

    fix

--- js/script.js
+++ js/script.js
@@ -1,80 +1,38 @@
-// скрипт для селекта позаимствован из интернета
-var x, i, j, l, ll, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    ll = selElmnt.length;
-    /* For each element, create a new DIV that will act as the selected item: */
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 1; j < ll; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function (e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
-            var y, i, k, s, h, sl, yl;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            sl = s.length;
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < sl; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    yl = y.length;
-                    for (k = 0; k < yl; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function (e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-    });
+var pic = document.getElementById('cat-img');
+
+function blank() {
+    pic.src = "../img/blank.jpg";
 }
 
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    var x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < xl; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-        }
-    }
+function domi() {
+    pic.src = "../img/cbg.jpg";
 }
+
+document.getElementById('acc1').addEventListener('click', blank);
+document.getElementById('acc2').addEventListener('click', blank);
+document.getElementById('acc3').addEventListener('click', blank);
+document.getElementById('acc4').addEventListener('click', blank);
+document.getElementById('acc5').addEventListener('click', blank);
+document.getElementById('acc6').addEventListener('click', blank);
+document.getElementById('acc7').addEventListener('click', blank);
+document.getElementById('acc8').addEventListener('click', blank);
+document.getElementById('acc9').addEventListener('click', blank);
+document.getElementById('acc10').addEventListener('click', blank);
+document.getElementById('acc11').addEventListener('click', blank);
+document.getElementById('acc12').addEventListener('click', domi);
+document.getElementById('acc13').addEventListener('click', blank);
+document.getElementById('acc14').addEventListener('click', blank);
+document.getElementById('acc15').addEventListener('click', blank);
+document.getElementById('acc16').addEventListener('click', blank);
+document.getElementById('acc17').addEventListener('click', blank);
+document.getElementById('acc18').addEventListener('click', blank);
+document.getElementById('acc19').addEventListener('click', blank);
+document.getElementById('acc20').addEventListener('click', blank);
+document.getElementById('acc21').addEventListener('click', blank);
+document.getElementById('acc22').addEventListener('click', blank);
+document.getElementById('acc23').addEventListener('click', blank);
+document.getElementById('acc24').addEventListener('click', blank);
+
 
 $(document).ready(function (event) {
     $('.header__burger').click(function (event) {
